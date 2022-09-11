5ccef43b261db81f618bf995fd448bc731000fed
Mateus Oliveira <66965232+mateusoliveira43@users.noreply.github.com>
2022-09-11 11:14:29 -0300

    feat: Add new Python and Poetry versions (#17)

--- scripts/generate_cli/config.py
+++ scripts/generate_cli/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from typing import Dict, List
 
-POETRY_VERSIONS: Dict[str, List[int]] = {"1.1": [14]}
-PYTHON_VERSIONS: Dict[str, List[int]] = {"3.10": [6]}
+POETRY_VERSIONS: Dict[str, List[int]] = {"1.1": [14, 15], "1.2": [0]}
+PYTHON_VERSIONS: Dict[str, List[int]] = {"3.10": [6, 7]}
 PYTHON_VARIATIONS: Dict[str, List[str]] = {
     "bullseye": ["", "-bullseye"],
     "slim-bullseye": ["-slim", "-slim-bullseye"],
