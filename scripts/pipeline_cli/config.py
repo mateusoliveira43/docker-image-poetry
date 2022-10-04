fd3f2f5b43ef6d82b7f46ea320fdbd763e9281f5
Mateus Oliveira <matews1943@gmail.com>
2022-10-04 09:42:23 -0300

    fixup! feat: Add quality checks for Python code

--- scripts/pipeline_cli/config.py
+++ scripts/pipeline_cli/config.py
@@ -3,8 +3,22 @@
 from pathlib import Path
 from typing import Dict, List
 
-POETRY_VERSIONS: Dict[str, List[int]] = {"1.1": [14, 15], "1.2": [0, 1]}
-PYTHON_VERSIONS: Dict[str, List[int]] = {"3.10": [6, 7]}
+POETRY_VERSIONS: Dict[str, List[int]] = {
+    "1.1": [
+        14,
+        15,
+    ],
+    "1.2": [
+        0,
+        1,
+    ],
+}
+PYTHON_VERSIONS: Dict[str, List[int]] = {
+    "3.10": [
+        6,
+        7,
+    ],
+}
 PYTHON_VARIATIONS: Dict[str, List[str]] = {
     "bullseye": ["", "-bullseye"],
     "slim-bullseye": ["-slim", "-slim-bullseye"],
